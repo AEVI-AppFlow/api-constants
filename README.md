e724e010e8e59f4573e74a795622164e6ed5d3f5
brettwold <brett@annalytics.co.uk>
2018-07-09 17:06:22 +0100

    Import from pos-flow-sdk

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# pos-flow-sdk-constants
+# POS Flow SDK Constants
+
+This library holds a set of constants that can be used with the AEVI POS Flow API.
+
+The use of this library is not mandatory and developers and acquirers are free to choose their own constants for use
+with POS Flow. 
---
+++ writegradleprops.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GRADLE_PROPERTIES_FILE="gradle.properties"
+OUTPUT_PROPS=${GRADLE_PROPERTIES_FILE}
+
+echo "Create gradle properties file"
+echo "org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" > ${OUTPUT_PROPS}
+echo "bintrayUser=$BINTRAY_USER" >> ${OUTPUT_PROPS}
+echo "bintrayKey=$BINTRAY_KEY" >> ${OUTPUT_PROPS}
