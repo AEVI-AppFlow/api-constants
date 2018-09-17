0c0914bcf55d09386ad896248c6b931cd470f85b
Andreas Borglin <andreas@jakoon.co.uk>
2018-09-17 09:45:26 +0100

    Added ServiceInfoDataKeys and bumped to 2.0.0-SNAPSHOT

---
+++ api-constants/src/main/java/com/aevi/sdk/flow/constants/ServiceInfoDataKeys.java
@@ -0,0 +1,8 @@
+package com.aevi.sdk.flow.constants;
+
+public interface ServiceInfoDataKeys {
+    String MERCHANTS = "merchants";
+    String SUPPORTS_MANUAL_ENTRY = "supportsManualEntry";
+    String PRINTS_RECEIPTS = "printsReceipts";
+    String REQUIRES_CARD_TOKEN = "requiresCardToken";
+}
