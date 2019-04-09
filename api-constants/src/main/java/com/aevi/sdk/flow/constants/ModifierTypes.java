7d3c38ca27e7835ef37b5b9479944b40e85a5dec
Andreas Borglin <andreas@jakoon.co.uk>
2019-04-09 14:09:13 +0100

    Bumped to 2.1.1

--- api-constants/src/main/java/com/aevi/sdk/flow/constants/ModifierTypes.java
+++ api-constants/src/main/java/com/aevi/sdk/flow/constants/ModifierTypes.java
@@ -5,8 +5,23 @@ package com.aevi.sdk.flow.constants;
  */
 public interface ModifierTypes {
 
+    /**
+     * For representing tax applied to a basket item.
+     */
     String MODIFIER_TYPE_TAX = "tax";
-    String MODIFIER_TYPE_PLUS = "plus";
-    String MODIFIER_TYPE_MINUS = "minus";
+
+    /**
+     * For extras added to an item, such as "Cheese" added to a "Burger" item.
+     */
+    String MODIFIER_TYPE_EXTRA = "extra";
+
+    /**
+     * The opposite of extra - a part of an item removed, such as a "Cheese Burger" without "Cheese".
+     */
+    String MODIFIER_TYPE_DEDUCTION = "deduction";
+
+    /**
+     * A discount applies to a basket item.
+     */
     String MODIFIER_TYPE_DISCOUNT = "discount";
 }
