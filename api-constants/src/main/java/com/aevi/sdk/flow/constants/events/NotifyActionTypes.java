0a11556efca9b8061a17c8ac67a9bcac3691383a
Brett Cherrington <brett@annalytics.co.uk>
2020-06-02 14:29:07 +0100

    Remove types not used in AppFlow

--- api-constants/src/main/java/com/aevi/sdk/flow/constants/events/NotifyActionTypes.java
+++ api-constants/src/main/java/com/aevi/sdk/flow/constants/events/NotifyActionTypes.java
@@ -1,16 +1,13 @@
 package com.aevi.sdk.flow.constants.events;
 
 /**
- * Non-exhaustive llist of
+ * Non-exhaustive list of notify action types that can be sent during a payment transaction
  */
 public interface NotifyActionTypes {
 
     String ABORT = "abort";
-    String CLOSE_BATCH_ = "closeBatch"; // not required for AppFlow should use "batchClose" flow
     String DUPLICATE_RECEIPT = "duplicateReceipt";
-    String DUPLICATE_RESPONSE = "duplicateResponse"; // TODO repeat response??
     String EMV_RECEIPT_PRINT = "emvReceiptPrint";
-    String HANDSHAKE = "handshake"; // TODO do we need this for AppFlow??
 
     String EFT_COMMUNICATION_FINISHED = "eftFinished";
     String COMMUNICATION_FINISHED = "communicationFinished";
