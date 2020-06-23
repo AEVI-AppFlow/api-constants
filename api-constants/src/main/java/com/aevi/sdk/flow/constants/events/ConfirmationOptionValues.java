f509ce21d55520b531316202a22b6ae60d0c5cd0
Brett Cherrington <brett@annalytics.co.uk>
2020-06-23 15:43:20 +0100

    Add option values for ok/cancel
    Add keys to hold receipt data

--- api-constants/src/main/java/com/aevi/sdk/flow/constants/events/ConfirmationOptionValues.java
+++ api-constants/src/main/java/com/aevi/sdk/flow/constants/events/ConfirmationOptionValues.java
@@ -8,4 +8,6 @@ public interface ConfirmationOptionValues {
     String CONFIRMATION_OPTION_REJECT = "reject";
     String CONFIRMATION_OPTION_YES = "yes";
     String CONFIRMATION_OPTION_NO = "no";
+    String CONFIRMATION_OPTION_OK = "ok";
+    String CONFIRMATION_OPTION_CANCEL = "cancel";
 }
